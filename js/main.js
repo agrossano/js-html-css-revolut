e1bb44bc5999076a33051812b2d871a8bb4e0a1f
Alessandro Grossano <grossano.alessandro@gmail.com>
2020-04-01 17:41:17 +0200

    Completo. Senza logica click outside.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function () {
 
+
+  $('.dropdown').hide();
+
+
+  //variabile parente dropdown
+  var p = $(this).parent();
+  $('.dropdown').not(p).hide;
+
+  $('li h3').click(function () {
+    $('.dropdown').hide();
+    $(this).siblings('.dropdown').toggle();
+  })
+
+  // $(window).click(function () {
+  //   $('.dropdown').hide();
+  // })
+
+
+  $(".fas").click(function () {
+    $(".hamburger-menu").show();
+  });
+
+  $(".close").click(function () {
+    $(".hamburger-menu").hide();
+  });
+
+
 });
